141b9519a2cffc47cfeb470f767edb302d47924b
Michael Majercin <44248126+Frostyface@users.noreply.github.com>
2022-07-29 12:01:34 -0700

    Add files via upload

---
+++ divisors.py
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+'''
+Create a program that asks the user for a number and then prints out a list of all 
+the divisors of that number. (If you don’t know what a divisor is, it is a number 
+that divides evenly into another number. For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
+'''
+
+number = int(input("Enter a number: "))
+
+numbers = list(range(1,number))
+
+
+for elm in numbers:
+    if number%elm == 0:
+        print(elm)
+
