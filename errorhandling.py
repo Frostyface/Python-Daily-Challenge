176db40dfe7db414377fb49bc1775b76b12473c2
Michael Majercin <44248126+Frostyface@users.noreply.github.com>
2022-08-01 11:21:03 -0700

    Add files via upload

---
+++ errorhandling.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+'''
+Generate a random number between 1 and 9 (including 1 and 9). Ask the user to guess 
+the number, then tell them whether they guessed too low, too high, or exactly right. 
+(Hint: remember to use the user input lessons from the very first exercise)
+
+Extras:
+
+Keep the game going until the user types “exit”
+Keep track of how many guesses the user has taken, and when the game ends, print this out.
+'''
+
+import random
+
+rand = random.randint(1,9)
+guesses = 0
+
+
+while True:
+
+    try:
+        guess = int(input("Enter a number between 1 and 9: "))
+    except ValueError:
+        print("Value Error is thrown.")
+
+    if guess > rand:
+        print("Guess is less than the number")
+        guesses += 1
+    elif guess < rand:
+        print("Guess is greater than number")
+        guesses += 1
+    elif guess == rand:
+        guesses += 1
+        print("Congrats! You found the number in " + str(guesses) + " guesses!")
+        break
+    else:
+        print("Unexpected input....")
+
